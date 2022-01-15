feb761311f0333158ce564c6c7b02dfb31e9e859
iair <dev.iair.918@gmail.com>
2022-01-15 10:10:20 +0900

    first commit

---
+++ src/main/java/qt/mate/qt_mate/QtMateApplication.java
@@ -0,0 +1,13 @@
+package qt.mate.qt_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qt/mate/qt_mate/QtMateApplicationTests.java
@@ -0,0 +1,13 @@
+package qt.mate.qt_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

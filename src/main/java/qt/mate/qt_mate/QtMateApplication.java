fb61346920550fa95908362dda217d85a6c5938c
iair <dev.iair.918@gmail.com>
2022-02-26 23:29:01 +0900

    add schedule

--- src/main/java/qt/mate/qt_mate/QtMateApplication.java
+++ src/main/java/qt/mate/qt_mate/QtMateApplication.java
@@ -1,13 +1,51 @@
 package qt.mate.qt_mate;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class QtMateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QtMateApplication.class, args);
 	}
+
+	@Scheduled(cron = "0 5 0 * * *")
+    public void GetQT() {
+		try{
+			URL url = new URL ("https://sum.su.or.kr:8888/Ajax/Bible/BodyBibleCont");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json; utf-8");
+			con.setRequestProperty("Accept", "application/json");
+			con.setDoOutput(true);
+			String jsonInputString ="{\"qt_ty\":\"QT6\",\"Base_de\":\"2022-02-26\",\"Bibletype\":\"1\"}";
+			try(OutputStream os = con.getOutputStream()) {
+				byte[] input = jsonInputString.getBytes("utf-8");
+				os.write(input, 0, input.length);
+			}
+			try(BufferedReader br = new BufferedReader(
+					new InputStreamReader(con.getInputStream(), "utf-8"))) {
+				StringBuilder response = new StringBuilder();
+				String responseLine = null;
+				while ((responseLine = br.readLine()) != null) {
+					response.append(responseLine.trim());
+				}
+				System.out.println(response.toString());
+			}
+			}catch(Exception e){
+				System.out.println("[EXCEPTION] "+e);
+			}
+    }
+    
 
 }
--- src/main/java/qt/mate/qt_mate/controller/DashboardController.java
+++ src/main/java/qt/mate/qt_mate/controller/DashboardController.java
@@ -1,10 +1,8 @@
 package qt.mate.qt_mate.controller;
-import java.io.IOException;
 
-import org.jsoup.Jsoup;
+import java.io.IOException;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,29 +25,29 @@ public class DashboardController {
         MyQtDTO myQtDTO = new MyQtDTO();
         MyPrayerDTO myPrayerDTO = new MyPrayerDTO();
         String id = "test";
-        try{
-            qtDTO = dasboardService.getQt();
-            jsonData.put("verse",qtDTO.getVerse());
-        }catch(NullPointerException e){
-            Document html = Jsoup.connect("https://sum.su.or.kr:8888/bible/today").get();
-            String verse = html.select(".bibleinfo_box").get(0).text();
-            int cutIndex = verse.indexOf('찬');
-            verse = verse.substring(5,cutIndex-1);
-            dasboardService.addQt(verse);
-            jsonData.put("verse", verse);
-        }
-        try{
-            myQtDTO = dasboardService.getMyQt(id);
-            jsonData.put("apply",myQtDTO.getContent());
-        }catch(NullPointerException e){
-            jsonData.put("apply", "");
-        }
-        try{
-            myPrayerDTO = dasboardService.getMyPrayer(id);
-            jsonData.put("prayer",myPrayerDTO.getContent());
-        }catch(NullPointerException e){
-            jsonData.put("prayer", "");
-        }
-        return jsonData.toString();
+        // try{
+        //     qtDTO = dasboardService.getQt();
+        //     jsonData.put("verse",qtDTO.getVerse());
+        // }catch(NullPointerException e){
+        //     Document html = Jsoup.connect("https://sum.su.or.kr:8888/bible/today").get();
+        //     String verse = html.select(".bibleinfo_box").get(0).text();
+        //     int cutIndex = verse.indexOf('찬');
+        //     verse = verse.substring(5,cutIndex-1);
+        //     dasboardService.addQt(verse);
+        //     jsonData.put("verse", verse);
+        // }
+        // try{
+        //     myQtDTO = dasboardService.getMyQt(id);
+        //     jsonData.put("apply",myQtDTO.getContent());
+        // }catch(NullPointerException e){
+        //     jsonData.put("apply", "");
+        // }
+        // try{
+        //     myPrayerDTO = dasboardService.getMyPrayer(id);
+        //     jsonData.put("prayer",myPrayerDTO.getContent());
+        // }catch(NullPointerException e){
+        //     jsonData.put("prayer", "");
+        // }
+        return "성공";
     }
 }
